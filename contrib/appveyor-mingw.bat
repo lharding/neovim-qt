65b72835f745a4fa1e5f463d9d4ff041295773e7
Rui Abreu Ferreira <raf-ep@gmx.com>
2016-11-01 12:57:29 +0000

    fixup

--- contrib/appveyor-mingw.bat
+++ contrib/appveyor-mingw.bat
@@ -9,3 +9,4 @@ mkdir build
 cd build
 cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../INSTALL ..
 cmake --build . --target install
+cmake --build . --target test -- VERBOSE=1
